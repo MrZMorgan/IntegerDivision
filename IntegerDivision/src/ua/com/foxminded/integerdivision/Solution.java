d4811eaeb7313ce5db8804b0a89ff7a6380436ed
Egor Anchutin <zbit16@gmail.com>
2020-11-05 20:10:44 +0300

    Add long division realization, add unit test for dividing two numbers and test for "0" divider;

--- IntegerDivision/src/ua/com/foxminded/integerdivision/Solution.java
+++ IntegerDivision/src/ua/com/foxminded/integerdivision/Solution.java
@@ -1,5 +1,17 @@
 package ua.com.foxminded.integerdivision;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+        int dividend = readDigit();
+        int divider = readDigit();
+        IntegerDivision integerDivision = new IntegerDivision();
+        System.out.println(integerDivision.makeDivision(dividend, divider));
+    }
+
+    public static int readDigit() {
+        Scanner scaner = new Scanner(System.in);
+        return scaner.nextInt();
+    }
 }
--- IntegerDivision/test/ua/com/foxminded/integerdivision/IntegerDivisionTest.java
+++ IntegerDivision/test/ua/com/foxminded/integerdivision/IntegerDivisionTest.java
@@ -2,13 +2,38 @@ package ua.com.foxminded.integerdivision;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class IntegerDivisionTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	private IntegerDivision integerDivision;
+	
+	private String divisionResult = "_654754│654\n" + 
+									" 654   │----\n" + 
+									" ---   │1001\n" + 
+									"   _754\n" + 
+									"    654\n" + 
+									"    ---\n" + 
+									"    100\n";
+	
+	@BeforeEach
+	void setUpIntegerDivision() {
+		integerDivision = new IntegerDivision();
 	}
 
+	@Test
+	void testLongDivideTwoNumbers() {
+		int dividend = 654754;
+		int divider = 654;
+		String actual = integerDivision.makeDivision(dividend, divider);
+		assertEquals(divisionResult, actual);
+	}
+	
+	@Test
+	void testDivideZero() {
+		int dividend = 123;
+		int divider = 0;
+		assertThrows(IllegalArgumentException.class, () -> integerDivision.makeDivision(dividend, divider));
+	}
 }
