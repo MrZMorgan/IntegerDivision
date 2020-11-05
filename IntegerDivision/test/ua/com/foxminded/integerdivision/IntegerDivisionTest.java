a68bf65c24318bb0f32770b524caae8d59518b0f
Egor Anchutin <zbit16@gmail.com>
2020-11-05 19:42:11 +0300

    Add classes, test for IntegerDivision class, create maven project

---
+++ IntegerDivision/test/ua/com/foxminded/integerdivision/IntegerDivisionTest.java
@@ -0,0 +1,14 @@
+package ua.com.foxminded.integerdivision;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IntegerDivisionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
