97c081591f13a042cac2a67439e13f3b0fab2a48
Egor Anchutin <zbit16@gmail.com>
2020-11-18 15:42:39 +0300

    Add tests with mocked LongDivisionCalculator

--- IntegerDivision/test/ua/com/foxminded/integerdivision/CalculatorTest.java
+++ IntegerDivision/test/ua/com/foxminded/integerdivision/CalculatorTest.java
@@ -9,6 +9,9 @@ import ua.com.foxminded.integerdivision.formatter.Formatter;
 import ua.com.foxminded.integerdivision.interfaces.Formatable;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -16,56 +19,93 @@ class CalculatorTest {
 	private int dividend;
 	private int divider;
 	private LongDivisionCalculator calculator;
+	private LongDivisionCalculator calculatorMock;
 	private Formatable formatterMock;
 	private Formatable formatter;
 	private CalculatorFacade facade;
 	private String result;
+	private CalculationDto divideTwoDigitsDto;
+	private CalculationDto dividendLessThanDividerDto;
+
+	private static String DIVIDE_TWO_DIGITS_EXPECTED_RESULT = "_654754|654\n" +
+															  " 654   -----\n" +
+															  " ---   |1001\n" +
+															  "   _754\n" +
+															  "    654\n" +
+															  "    ---\n" +
+															  "    100\n";
+
+	private static final String DIVIDEND_LESS_THEN_DIVIDER_EXPECTED_RESULT = "0 / 123 = 0";
 
 	@BeforeEach
 	void setUp() {
 		calculator = new LongDivisionCalculator();
-		formatterMock = Mockito.mock(Formatter.class);
-		facade = new CalculatorFacade(calculator, formatterMock);
+		calculatorMock = mock(LongDivisionCalculator.class);
+		formatter = new Formatter();
+		formatterMock = mock(Formatter.class);
 	}
 
 	@Test
-	void divideTwoNumbersShouldEqualsDto() {
+	void divideTwoNumbersWithMockingFormatterShouldEqualsDto() {
 		dividend = 654754;
 		divider = 654;
+		int divisionResult = 1001;
+		int remainder = 100;
+		List<Integer> intermediateDividends = new ArrayList<>();
+		List<Integer> intermediateDividers = new ArrayList<>();
+		List<Integer> zerosBeforeDividends = new ArrayList<>();
+		List<Integer> zerosBeforeDividers = new ArrayList<>();
 
+		intermediateDividends.add(654);
+		intermediateDividends.add(754);
+
+		intermediateDividers.add(654);
+		intermediateDividers.add(654);
+
+		zerosBeforeDividends.add(0);
+		zerosBeforeDividends.add(3);
+
+		zerosBeforeDividers.add(1);
+		zerosBeforeDividers.add(4);
+
+		CalculationDto actualDto = new CalculationDto(dividend, divider, remainder, divisionResult);
+		actualDto.setIntermediateDividends(intermediateDividends);
+		actualDto.setIntermediateDividers(intermediateDividers);
+		actualDto.setZerosBeforeDividends(zerosBeforeDividends);
+		actualDto.setZerosBeforeDividers(zerosBeforeDividers);
+
+		facade = new CalculatorFacade(calculator, formatterMock);
 		facade.divide(dividend, divider);
 
 		ArgumentCaptor<CalculationDto> captor = ArgumentCaptor.forClass(CalculationDto.class);
 		verify(formatterMock).createResult(captor.capture());
 
 		CalculationDto capturedDto = captor.getValue();
-		CalculationDto actualDto = calculator.longDivision(dividend, divider);
 
 		assertEquals(actualDto, capturedDto);
 	}
 
 	@Test
-	void zeroDividendShouldEqualsDto() {
+	void zeroDividendWithMockingFormatterShouldEqualsDto() {
 		dividend = 0;
 		divider = 123;
 
+		CalculationDto actualDto = new CalculationDto(dividend, divider, 0, 0);
+
+		facade = new CalculatorFacade(calculator, formatterMock);
 		result = facade.divide(dividend, divider);
 
 		ArgumentCaptor<CalculationDto> captor = ArgumentCaptor.forClass(CalculationDto.class);
 		verify(formatterMock).createResult(captor.capture());
 
 		CalculationDto capturedDto = captor.getValue();
-		CalculationDto actualDto = calculator.longDivision(dividend, divider);
 
 		assertEquals(actualDto, capturedDto);
 	}
 
-
-
 	@Test
 	void nullCalculationDtoShouldThrowNullPointerException() {
 		String expectedErrorMessage = "DTO cannot be \"null\"";
-		formatter = new Formatter();
 
 		assertThrows(NullPointerException.class, () -> formatter.createResult(null));
 
@@ -82,7 +122,6 @@ class CalculatorTest {
 	@Test
 	void emptyCalculationDtoShouldThrowNullPointerException() {
 		String expectedErrorMessage = "DTO cannot be empty";
-		formatter = new Formatter();
 
 		CalculationDto emptyDto = new CalculationDto(0, 0, 0, 0);
 
@@ -106,6 +145,7 @@ class CalculatorTest {
 		divider = 0;
 		String expectedErrorMessage = "Can`t divide by zero";
 
+		facade = new CalculatorFacade(calculator, formatter);
 		assertThrows(IllegalArgumentException.class, () -> facade.divide(dividend, divider));
 
 		String actualErrorMessage = "";
@@ -124,7 +164,6 @@ class CalculatorTest {
 		divider = 654;
 
 		LongDivisionCalculator calculatorMock = mock(LongDivisionCalculator.class);
-		formatterMock = mock(Formatter.class);
 
 		CalculationDto dto = new LongDivisionCalculator().longDivision(dividend, divider);
 		String resultOfDivision = new Formatter().createResult(dto);
@@ -141,5 +180,61 @@ class CalculatorTest {
 
 		inOrder.verify(calculatorMock).longDivision(dividend, divider);
 		inOrder.verify(formatterMock).createResult(dto);
+	}
+
+	@Test
+	void divideTwoNumbersWithMockingCalculatorShouldEqualsDto() {
+		dividend = 654754;
+		divider = 654;
+		int divisionResult = 1001;
+		int remainder = 100;
+
+		List<Integer> intermediateDividends = new ArrayList<>();
+		List<Integer> intermediateDividers = new ArrayList<>();
+		List<Integer> zerosBeforeDividends = new ArrayList<>();
+		List<Integer> zerosBeforeDividers = new ArrayList<>();
+
+		intermediateDividends.add(654);
+		intermediateDividends.add(754);
+
+		intermediateDividers.add(654);
+		intermediateDividers.add(654);
+
+		zerosBeforeDividends.add(0);
+		zerosBeforeDividends.add(3);
+
+		zerosBeforeDividers.add(1);
+		zerosBeforeDividers.add(4);
+
+		CalculationDto actualDto = new CalculationDto(dividend, divider, remainder, divisionResult);
+		actualDto.setIntermediateDividends(intermediateDividends);
+		actualDto.setIntermediateDividers(intermediateDividers);
+		actualDto.setZerosBeforeDividends(zerosBeforeDividends);
+		actualDto.setZerosBeforeDividers(zerosBeforeDividers);
+
+		facade = new CalculatorFacade(calculatorMock, formatter);
+
+		when(calculatorMock.longDivision(dividend, divider))
+				.thenReturn(actualDto);
+
+		String result = facade.divide(dividend, divider);
+
+		assertEquals(DIVIDE_TWO_DIGITS_EXPECTED_RESULT, result);
+	}
+
+	@Test
+	void zeroDividendWithMockingCalculatorShouldEqualsDto() {
+		dividend = 0;
+		divider = 123;
+		CalculationDto actualDto = new CalculationDto(dividend, divider, 0, 0);
+
+		facade = new CalculatorFacade(calculatorMock, formatter);
+
+		when(calculatorMock.longDivision(dividend, divider))
+				.thenReturn(actualDto);
+
+		String result = facade.divide(dividend, divider);
+
+		assertEquals(DIVIDEND_LESS_THEN_DIVIDER_EXPECTED_RESULT, result);
 	}
 }
